2a47a749b261fc53221246bf99d5cc04f71fec0e
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-04-13 15:31:19 +0200

    Update BootReceiver.kt

--- app/src/main/java/com/mohamedabdelazeim/zekr/service/BootReceiver.kt
+++ app/src/main/java/com/mohamedabdelazeim/zekr/service/BootReceiver.kt
@@ -1,1 +1,15 @@
-اا
+package com.mohamedabdelazeim.zekr.receiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import com.mohamedabdelazeim.zekr.service.ZekrService
+
+class BootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
+            val serviceIntent = Intent(context, ZekrService::class.java)
+            context.startForegroundService(serviceIntent)
+        }
+    }
+}
